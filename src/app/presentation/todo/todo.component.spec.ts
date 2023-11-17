908e553470f2315ca90f4bcbf58f868a1fe8d560
Jocinei Dalla Costa <jocineidallacosta@hotmail.com>
2023-11-17 16:19:23 -0300

    FEAT: add todo component test

--- src/app/presentation/todo/todo.component.spec.ts
+++ src/app/presentation/todo/todo.component.spec.ts
@@ -1,21 +1,57 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TodoComponent } from './todo.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TodoGetAllUseCase } from 'src/app/domain/use-cases/todo/todo-get-all/todo-get-all-use-case';
 
 describe('TodoComponent', () => {
   let component: TodoComponent;
   let fixture: ComponentFixture<TodoComponent>;
+  let todoGetAllUseCase: TodoGetAllUseCase;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TodoComponent]
+      declarations: [TodoComponent],
+      imports: [FormsModule, ReactiveFormsModule],
     });
-    fixture = TestBed.createComponent(TodoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create component', () => {
+    fixture = TestBed.createComponent(TodoComponent);
+    expect(fixture.componentInstance).toBeDefined();
   });
+
+  it('should show H4 title', () => {
+    fixture = TestBed.createComponent(TodoComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h4')?.textContent).toContain('Cadastro de To-do(s)');
+  });
+
+  it('should not show H4 todo list when dont todo', () => {
+    fixture = TestBed.createComponent(TodoComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    todoGetAllUseCase = TestBed.inject(TodoGetAllUseCase);
+    spyOn(todoGetAllUseCase, 'handle').and.returnValue([]);
+    expect(compiled.querySelector('h4')?.textContent).not.toContain('Lista de To-do(s)');
+  });
+
+
+  it('should show H4 todo list when have todo', () => {
+    const todoMock = [{
+        id: '1',
+        isDone: false,
+        description: 'todo 1'
+    }];
+    fixture = TestBed.createComponent(TodoComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+    fixture.componentInstance.todos = todoMock;
+    fixture.detectChanges();
+    expect(compiled.querySelector('h5')?.textContent).toContain('Lista de To-do(s)');
+  });
+
+  
+
+
+
 });
